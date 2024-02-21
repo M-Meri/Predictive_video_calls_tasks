3873b5433374ee353bf125ccabb4b85881a33dae
M-Meri <meri.margaryan.y@gmail.com>
2024-02-21 14:19:02 +0400

    task2

---
+++ Types_and_Variables/main.cpp
@@ -0,0 +1,16 @@
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    int integer = 5;
+    float fp_number = 3.14f;
+    double dfp_number = 2.5;
+    char character = 'a';
+    bool flag = true;
+
+    qDebug() << integer << ' ' << fp_number << ' ' << dfp_number << ' ' << character << ' ' << flag << '\n';
+
+    return a.exec();
+}
